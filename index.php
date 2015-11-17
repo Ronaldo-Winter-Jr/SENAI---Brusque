0c6dd9056e33e4ce0796d6e294a41c1d1a9aa303
Ronaldo-Winter-Jr <>
2015-11-17 00:25:54 -0200

    closes #3

--- index.php
+++ index.php
@@ -2,10 +2,25 @@
     <head>
         <meta charset="UTF-8">
         <title>Site Institucional do SENAI - Brusque</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/estilo.css">
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php
-        
-        ?>
+        <div class="container-fluid">
+            <header class="jumbotron" id="topo">
+                <h1>Bootstrap</h1>
+                <p>Aloha mundo do Bootstrap</p>
+                <form role="form">
+                    <div class="form-group">
+                      <input type="email" class="form-control" id="email" placeholder="E-mail">
+                      <span class="glypiicon glyphicon-envelope"></span>
+                      </br>
+                      <button type="submit" class="btn btn-default">Cadastrar</button>
+                </form>
+                    </div>  
+            </header>
+        </div>    
     </body>
 </html>
