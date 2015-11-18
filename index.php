9280d4422380b806f1e880b38da8e34dd7305a22
Ronaldo Winter Junior <>
2015-11-18 20:34:44 -0200

    Closes #18 Closes #30

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
+<!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <title>Site Institucional do SENAI - Brusque</title>
+
+        <!-- Fonte -->
+        <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 
         <!-- CSS -->
         <link rel="stylesheet" href="css/bootstrap.min.css" />
@@ -11,27 +15,31 @@
         <script src="js/jquery-1.11.3.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
 
+
     </head>
+
     <body data-spy="scroll" data-target=".navbar" data-offset="50">
+
         <header class="jumbotron text-center" id="topo">
             <h1>SENAI/SC Brusque</h1>
-            <p>Alo Ha mundo SENAI</p>
-            <form role="form">
-                <div class="form-inline">
-                    <div class="input-group input-group-sm">
-                        <span class="input-group-addon">
-                            <span class="glyphicon glyphicon-envelope"></span>
-                        </span>
+            <p>Alo Ha mundo do Bootstrap</p>
 
-                        <input type="email"  class="form-control" id="email"  placeholder="seunome@provedor.com.br">                            
-                    </div>
-                    <button type="submit" class="btn btn-success">
-                        Cadastrar
-                    </button>       
+            <form role="form" class="form-inline">
+                <!-- Gerador de Bootstrap Form 
+                     http://embed.plnkr.co/ZrnnkFR3Yv9HGQHrRMiW/preview -->
+                <!-- Em caso de sucesso na validação: <div class="form-group has-feedback has-success"> -->
+                <!-- Em caso de falha na validação: <div class="form-group has-feedback has-error"> -->
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="seunome@provedor.com.br" size="40px"/>
+                    <i class="form-control-feedback glyphicon glyphicon-envelope"></i>
                 </div>
+                <button type="submit" class="btn btn-success">Cadastrar</button>
             </form>
         </header>
-        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
+
+        <!-- Barra de Navegação -->
+
+        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="250">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -44,59 +52,149 @@
                 <div>
                     <div class="collapse navbar-collapse" id="myNavbar">
                         <ul class="nav navbar-nav">
-                            <li><a href="#section1">Início</a></li>
-                            <li><a href="#section2">Cursos</a></li>
-                            <li><a href="#section3">Notícias</a></li>
-                            <li><a href="#section4">Fotos</a></li>
-                            <li><a href="#section5">Vídeos</a></li>
-                            <li><a href="#section6">Contatos</a></li>
-                            <li><a href="#section7">Localização</a></li>
-                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
-                                <ul class="dropdown-menu">
+                            <li><a href="#inicio">Início</a></li>
+                            <li><a href="#cursos">Cursos</a></li>
+                            <li><a href="#noticias">Notícias</a></li>
+                            <li><a href="#fotos">Fotos</a></li>
+                            <li><a href="#videos">Vídeos</a></li>
+                            <li><a href="#contato">Contato</a></li>
+                            <li><a href="#local">Localização</a></li>
 
+                            <!-- Menu drop-down
+                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#section41">Section 4-1</a></li>
+                                    <li><a href="#section42">Section 4-2</a></li>
                                 </ul>
                             </li>
+                            -->
                         </ul>
                     </div>
                 </div>
             </div>
         </nav>    
+        <!-- Fim da Barra de Navegação -->
 
-        <div id="section1" class="container-fluid">
-            <h1>Início</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section2" class="container-fluid">
+        <section id="inicio" class="container-fluid">
+
+            <!-- Carrossel de imagens -->
+            <div clas="row">                
+                <!--
+                Uma linha dividida em 12 colunas, sendo uma coluna central
+                tamanho 8 e um distanciamento de 2
+                
+                Sistema de Grid no Bootstrap
+                http://www.w3schools.com/bootstrap/bootstrap_grid_examples.asp
+                -->
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-pause="hover" data-interval="2000">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                            <li data-target="#myCarousel" data-slide-to="3"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="img/saiba_mais_sobre_o_senai.png" alt="Chania">
+                            </div>
+
+                            <div class="item">
+                                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=460%C3%97365&w=460&h=365" alt="Chania">
+                            </div>
+
+                            <div class="item">
+                                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=460%C3%97365&w=460&h=365" alt="Flower">
+                            </div>
+
+                            <div class="item">
+                                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=460%C3%97365&w=460&h=365" alt="Flower">
+                            </div>
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div> <!-- row -->
+            <!-- fim do carrossel de imagens -->
+        </section>
+
+        <section id="cursos" class="container-fluid">
             <h1>Cursos</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section3" class="container-fluid">
+        </section>
+
+        <section id="noticias" class="container-fluid">
             <h1>Notícias</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section4" class="container-fluid">
+        </section>
+
+        <section id="fotos" class="container-fluid">
             <h1>Fotos</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section5" class="container-fluid">
-            <h1>Vídeos</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section6" class="container-fluid">
-            <h1>Contatos</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section7" class="container-fluid">
-            <h1>Localização</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-    <script src="js/meuscript.js"></script>   
+        </section>
+
+        <section id="videos" class="container-fluid">
+            <div class="text-center">
+                <h1>Galeria Vídeos</h1>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLXTxMOByxQKOZcxIvbgx_93ItPjnbqYIk" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </section>
+
+        <section id="contato" class="container-fluid">
+            <h1>Contato</h1>
+            <div class="row">
+                <div class="col-md-4">
+                    <p>Entre em contato, responderemos nas próximas 24 horas</p>
+                    <p><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>
+                        Brusque/SC
+                    </p>
+                    <p>
+                        <i class="glyphicon glyphicon-phone-alt" aria-hidden="true"></i>
+                        +55 (47) 3251-8900
+                    </p>
+                    <p>
+                        <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
+                        brusque@fiesc.com.br
+                    </p>
+                </div>
+                <div class="col-md-8">
+                    <form>
+                        <div class="form-inline">
+                                <input type="text" class="form-control" id="cNome" placeholder="Nome">
+                                <input type="email" class="form-control" id="cEmail" placeholder="E-mail">
+                        </div>
+                        <textarea class="form-control" rows="5" cols="40" id="cMensagem" placeholder="Mensagem"></textarea>
+                        <button type="submit" class="btn btn-default">Enviar</button>
+                    </form>
+                </div>
+            </div>
+
+
+        </section>
+
+        <section id="local" class="container-fluid">
+            <div class="text-center">
+                <h1>Localização</h1>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d5972.578301415663!2d-48.91459628134629!3d-27.11484556231412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x94df47b2050e1221%3A0xb0ebeaaf853b447!2sAv.+Primeiro+de+Maio%2C+670+-+Primeiro+de+Maio%2C+Brusque+-+SC%2C+88353-202!3m2!1d-27.1156975!2d-48.910202!5e0!3m2!1spt-BR!2sbr!4v1447800510027" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </section>
+
+        <script src="js/meuscript.js"></script>
+
     </body>
 </html>
--- js/meuscript.js
+++ js/meuscript.js
@@ -1,5 +1,5 @@
 // Add scrollspy to <body>
-$('body').scrollspy({target: ".navbar", offset: 50}); 
+//$('body').scrollspy({target: ".navbar", offset: 50}); 
 
 // Add smooth scrolling to all links inside a navbar
 $("#myNavbar a").on('click', function(event){
@@ -9,14 +9,16 @@ $("#myNavbar a").on('click', function(event){
 
   // Store hash (#)
   var hash = this.hash;
+  //console.log("Hash: " + hash);
+  //console.log($(hash).offset().top);
 
   // Using jQuery's animate() method to add smooth page scroll
   // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
   $('html, body').animate({
     scrollTop: $(hash).offset().top
-  }, 1200, function(){
+  }, 800, function(){
 
-    // Add hash (#) to URL when done scrolling (default click behavior)
+    // Add hash (as the same as #) to URL when done scrolling (default click behavior)
     window.location.hash = hash;
   });
 });
